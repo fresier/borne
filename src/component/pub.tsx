2b0e954d2e4a3d00c08937b62690425a573ab359
Frédéric RESIER <frederic.resier@ulb.be>
2024-01-30 11:42:30 +0100

    annonce by monpsy

--- src/component/pub.tsx
+++ src/component/pub.tsx
@@ -1,41 +1,56 @@
+import { useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
 
 //position-absolute top-50 start-50 translate-middle
 
-
 export default function Pub() {
-    const timer = 5000
+  const timer = 5000;
 
-    return (<>
-        <div 
-            id='pub'
-            style={{
-                'position':'absolute',
-                'zIndex':'50',
-                //'top':'calc((100vh - 20px - 20px)/2)',
-                'top':'0',
+  const [data, setData] = useState([]); //data from api
 
-                'width':'100%',
-                'height':'100vh',
-                'lineHeight':'20px',
-                //'backgroundColor':'#1F2A2E'
-            }}>
-    <div className='m-3'> 
-      
-    <Carousel>
-      <Carousel.Item interval={timer}>
-        <img className="img-thumbnail carousel-img" alt='help ?' width="100%" src='/assets/img/FAQ_2021_WebFac.jpg' />
-      </Carousel.Item>
-      <Carousel.Item interval={timer}>
-        <img className="img-thumbnail carousel-img" alt='help ?' width="100%" src='/assets/img/FAQ_2021_WebFac.jpg' />
-      </Carousel.Item>
-      <Carousel.Item interval={timer}>
-        <img className="img-thumbnail carousel-img" alt='help ?' width="100%" src='/assets/img/FAQ_2021_WebFac.jpg' />
-      </Carousel.Item>
-    </Carousel>
+  //load images grom api
+  useEffect(() => {
+    fetch("https://monpsy.ulb.be/ajax/annonce.php")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
 
-            </div>
+  return (
+    <>
+      {data && (
+        <div
+          id="pub"
+          style={{
+            position: "absolute",
+            zIndex: "50",
+            //'top':'calc((100vh - 20px - 20px)/2)',
+            top: "0",
+
+            width: "100%",
+            height: "100vh",
+            lineHeight: "20px",
+            //'backgroundColor':'#1F2A2E'
+          }}
+        >
+          <div className="m-3">
+            <Carousel>
+              {data.map((item) => (
+                <Carousel.Item interval={timer}>
+                  <img
+                    className="img-thumbnail carousel-img"
+                    alt="help ?"
+                    width="100%"
+                    src={item}
+                  />
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </div>
         </div>
-
-    </>)
+      )}
+    </>
+  );
 }
